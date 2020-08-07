3a8ded213c619bba4a668864cd65b7b359ee9ad2
keshavdulal <keshav.dulal@gmail.com>
2020-08-07 22:21:59 +0545

    dev : complete CRUD apis for exercise.js

--- backend/routes/exercise.js
+++ backend/routes/exercise.js
@@ -4,14 +4,14 @@ const router = require('express').Router();
 //using models
 let Exercise = require('../models/exercise.model');
 
-//make get request happen for /users/ endpoint
+// GET REQUEST (ALL)
 router.route('/').get((req,res) => {
     Exercise.find() //mongoose query or method
     .then(exercises => res.json(exercises))
-    .catch(err => res.status(400).json('Error: '+ err));
+    .catch(err => res.status(400).json('Error: ' + err));
 }); 
 
-//make post request happen for /users/add endpoint
+// POST REQUEST
 router.route('/add').post((req,res) => {
     const username = req.body.username;
     const description = req.body.description;
@@ -27,7 +27,38 @@ router.route('/add').post((req,res) => {
 
     newExercises.save() //mongoose query or method
     .then(() => res.json('Exercise Added'))
-    .catch(err => res.status(400).json('Error: '+ err));
+    .catch(err => res.status(400).json('Error: ' + err));
 })
+
+// GET REQUEST (by id)
+router.route('/:id').get((req,res) => {
+    Exercise.findById(req.params.id) //mongoose query or method
+    .then(exercises => res.json(exercises))
+    .catch(err => res.status(400).json('Error: ' + err));
+}); 
+
+// DELETE REQUEST (by id)
+router.route('/:id').delete((req,res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Exercise Deleted'))
+    .catch(err => res.status(400).json('Error: ' + err));
+})
+
+// POST REQUEST (update by id)
+router.route('/update/:id').post((req,res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => {
+        exercise.username = req.body.username,
+        exercise.description = req.body.description,
+        exercise.duration = Number(req.body.duration),
+        exercise.date = Date.parse(req.body.date)
+
+        exercise.save()
+        .then(() => res.json('Exercise updated'))
+        .catch(err => res.status(400).json('Error: ' + err))
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+})
+
 
 module.exports = router;
