da8daf2590ad19f62c1cc2e4270b0ec27283903a
keshavdulal <keshav.dulal@gmail.com>
2020-08-07 20:35:00 +0545

    fix typo on exercise model

--- backend/models/exercise.model.js
+++ backend/models/exercise.model.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-//making a exercise schema with just four fields and then validating it
+//making a exercise schema with just four  fields and then validating it
 const exerciseSchema = new Schema({
     username:{type:String, required:true},
-    description:{type:String, rquired: true},
-    duration:{type:Number, rquired: true},
-    date:{type:Date, rquired: true},
+    description:{type:String, required: true},
+    duration:{type:Number, required: true},
+    date:{type:Date, required: true},
 },{
     timestamps:true,    // keep track of creation & modification
 });
