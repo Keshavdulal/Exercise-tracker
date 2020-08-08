457af50d4bebc12e2d1ee47dca2aa63108e91534
keshavdulal <keshav.dulal@gmail.com>
2020-08-08 10:32:26 +0545

    dev : add routes to components in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from "./components/navbar.component";
+import ExerciseList from "./components/exercise-list.component";
+import EditExercise from "./components/edit-exercises.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
-    <div className="container">
-      Hello World
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar/>
+        <br/>
+        <Route path="/" exact component={ExerciseList}/>
+        <Route path="/edit/:id" exact component={EditExercise}/>
+        <Route path="/create" exact component={CreateExercise}/>
+        <Route path="/user" exact component={CreateUser}/>
+      </div>
+    </Router>
   );
 }
 
